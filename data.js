36dbcfcaa4323f896af571e0d78ae13bc8ee3718
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-02-04 16:48:27 +0545

    Update data.js

--- data.js
+++ data.js
@@ -1,38 +1,38 @@
 const initialQuotes = [
     {
         id: '6',
-        text: "Infrastructure Development मात्र Development होइन,  Environment Protection पनि Development हो। ",
-        author: "Pleading Statement in 081-WC-0002 case on 2081/07/14 in संवैधानिक इजलास",
+        text: "",
+        author: "",
         favoriteType: 0
     },
     {
         id: '5',
-        text: "२०८१ को महाकुम्भस्नान खतरा र खतरनाक दुवै भयो ",
-        author: "रित्तिका शर्मा",
+        text: "",
+        author: "",
         favoriteType: 0
     },
     {
         id: '4',
-        text: "बचपनको प्रभुत्व गर, युवापाको शुद्ध गर र बुढापाको सिद्ध गर ",
-        author: "स्वामि रामभद्राचार्य",
+        text: " ",
+        author: "",
         favoriteType: 0
     },
     {
         id: '3',
-        text: "अब राजनीतिको restructuring & reengineering  आवश्यक छ",
-        author: "डा. बाबुराम भट्टराई (ok khabar 2081 माघ २०)",
+        text: "",
+        author: "",
         favoriteType: 0
     },
     {
         id: '2',
-        text: "जसले अज्ञानबाट बचाउँछ ऊ ब्रह्माण हो, जसले अन्यायबाट बचाउँछ ऊ क्षत्रीय हो, जसले अभावबाट बचाउँछ ऊ वैश्य हो र जसले अपवित्रताबाट बचाउँछ ऊ शुद्र हो",
-        author: "स्वामि रामदेव",
+        text: "",
+        author: "",
         favoriteType: 0
     },
     {
         id: '1',
-        text: "",
-        author: "",
+        text: "यी प्रश्नहरूको समुचित विश्लेषण र निरूपण गरी फैसलामा समावेश गर्दा फैसलाको गुणस्तर र विश्वसनीयता अभिवृद्धि हुने गरी तयार गर्नुहोस्।",
+        author: "ठहर खण्ड लेखनको लागि",
         favoriteType: 0
     }
 ];
